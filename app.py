36c1f4a457b56e624afa8a1b453e747a2d184e84
James Brind <jb753@cam.ac.uk>
2021-12-15 19:44:46 +0000

    Move to URL input, improve frontend javascript

--- app.py
+++ app.py
@@ -5,18 +5,20 @@ app = Flask(__name__)
 
 word_list = jabbrev.WordList('ltwa_eng.csv')
 
-@app.route('/',methods=['GET', 'POST'])
-def index():
-
-    if request.method == 'POST':
-        long_title = request.form['long_title']
-        short_title = jabbrev.abbreviate(long_title,word_list)
-    else:
-        short_title = ""
-        long_title = ""
-
+@app.route('/<journal>')
+def abbreviate(journal):
+    short_title = jabbrev.abbreviate(journal,word_list)
     return render_template(
             'index.html',
             short_title=short_title,
-            long_title=long_title,
+            long_title=journal,
+            )
+
+@app.route('/')
+def index():
+
+    return render_template(
+            'index.html',
+            short_title="",
+            long_title="",
             )
